bd948e5bfbb4437b61979d608a5c8b0be16a5afe
Matias Banzas <bathan@gmail.com>
2015-05-20 11:41:03 -0300

    Added PHPMailer Lib

--- include/config.local.php
+++ include/config.local.php
@@ -6,3 +6,9 @@ define('_APP_URL', 'http://congreso.ttt.com.ar/');
 define('_INSTALL_URL', 'http://congreso.tttt.com.ar/');
 
 define("_DB_HOST","localhost");
+
+
+//-- Datos Hosting
+define('_DB_NAME','241238_congreso');
+define('_DB_USER','241238-congreso');
+define('_DB_PASS','6pUXxCnC');
