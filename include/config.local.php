2b553ce6612b059bc132b9f1eaa04252993fcc59
Matias Banzas <bathan@gmail.com>
2015-05-21 11:44:13 -0300

    Bla bla config.local

--- include/config.local.php
+++ include/config.local.php
@@ -9,3 +9,13 @@ define('_INSTALL_URL', 'http://congreso.tttt.com.ar/');
 
 define("_DB_HOST","localhost");
 
+//-- HOSTING
+/*
+define("_APP_PATH","/webcp/docs/webs/241238/www");
+define('_APP_URL', 'http://www.congresoutelpa.com.ar/');
+define('_INSTALL_URL', 'http://www.congresoutelpa.com.ar/');
+
+define('_DB_NAME','241238_congreso');
+define('_DB_USER','241238-congreso');
+define('_DB_PASS','6pUXxCnC');
+*/
